b543f5ed08ef3ab575b88044e3c859296d5c3d24
RysKowalski <ryskowalski0@gmail.com>
2025-03-24 19:46:53 +0100

    f

---
+++ interpreter.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+class Interpreter {
+private:
+        std::vector<std::vector<std::string>> program
+                int A
+                        int B                                                                            int C
+                                int D
+                                        unsigned current_line
+                                                unsigned steps
+
+                                                public:
+                                                        Interpreter(std::vector<std::vector<std::string>> p)
+
+                                                        }
+
+                                                                                                                                         int main() {
+
+                                                                                                                                                 }
